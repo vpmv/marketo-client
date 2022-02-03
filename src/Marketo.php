d85676aef7aaf4dd59244ea59b2655309ba56026
Valentijn Verhallen <contact@valentijn.co>
2022-02-03 20:03:07 +0100

    Add method to get MarketoClient

--- src/Marketo.php
+++ src/Marketo.php
@@ -22,6 +22,14 @@ class Marketo
         return new static($client);
     }
 
+    /**
+     * @return \Netitus\Marketo\Client\MarketoClientInterface
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
     public function programs()
     {
         return new API\Programs($this->client);
